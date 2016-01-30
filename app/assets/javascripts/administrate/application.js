67af38dbc40294e1a19c67d787f5a21c3d80f0de
Rafael Gióia de Barros <rafaelgioia@globo.com>
2016-01-30 17:07:21 -0200

    Add bootstrap

--- app/assets/javascripts/administrate/application.js
+++ app/assets/javascripts/administrate/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
-//= require bootstrap.min
+//= require bootstrap
 //= require selectize
 //= require moment
 //= require datetime_picker
